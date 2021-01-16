f1500e14f2ea4cc07bd8b7c84633d4a408d743c8
niujin <niujin@gmail.com>
2021-01-16 20:39:03 +0800

    ul padding, margins etc

--- static/components/Main.js
+++ static/components/Main.js
@@ -40,9 +40,9 @@ class Main extends React.Component {
             <div className="column">
                 <div className="container">
                     <nav className="tabs is-full">
-                            <a className={me.props.view == Constants.VIEW_EVENT ? "active" : ""} onClick={this.handleClick} tab="event">Events</a>
-                            <a className={me.props.view == Constants.VIEW_BIRTH ? "active" : ""} onClick={this.handleClick} tab="birth">Births</a>
-                            <a className={me.props.view == Constants.VIEW_DEATH ? "active" : ""} onClick={this.handleClick} tab="death">Deaths</a>
+                        <a className={me.props.view == Constants.VIEW_EVENT ? "active" : ""} onClick={this.handleClick} tab="event">Events</a>
+                        <a className={me.props.view == Constants.VIEW_BIRTH ? "active" : ""} onClick={this.handleClick} tab="birth">Births</a>
+                        <a className={me.props.view == Constants.VIEW_DEATH ? "active" : ""} onClick={this.handleClick} tab="death">Deaths</a>
                     </nav>
                 </div>
                 <section class="section tab-content-container" id="mainTabs">
